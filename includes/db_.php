1b54d99f21a661a26be01b5825e241eeb0f5cc59
kaizoku010 <dixontheworldvsy@gmail.com>
2025-03-07 16:32:14 +0300

    rework ui 0.4

--- includes/db_.php
+++ includes/db_.php
@@ -1,21 +1,17 @@
 <?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 $servername = "localhost:3306";
-$username   = "kwetzigc_luke";
-$password   = "{n0P.7e0a3t2";
-$dbname     = "kwetzigc_db2";
+$username = "kwetzigc_luke";
+$password = "{n0P.7e0a3t2";
+$dbname = "kwetzigc_db2";
 
-try {
-    $conn = new mysqli($host, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    if ($conn->connect_error) {
-        throw new Exception("Connection failed: " . $conn->connect_error);
-    }
-} catch (Exception $e) {
-    die("Database connection error: " . $e->getMessage());
+if ($conn->connect_error) {
+    echo ("<script>console.log('connection failed');</script>");
+
+    die("Connection failed: " . $conn->connect_error);
 }
 
-echo("<script>console.log('connection set');</script>");
+echo ("<script>console.log('connection set');</script>");
+
+?>
